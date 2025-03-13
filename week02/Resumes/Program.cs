bdbae96278a01febd9bf2cbda6bb48af8389ae49
Santiago Bergerat <santiagobergerat@gmail.com>
2025-03-13 19:56:25 -0300

    added classes Job And Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,35 @@
-using System;
+using Resumes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+        job1.Display();
+
+
+        Job job2 = new Job();
+
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+        job2.Display();
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Santiago Bergerat";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
